0b9670ee4d11dd7c5ee197a7dc4da44e2d7b2624
Bob-Coding <bobgunsing@gmail.com>
2020-01-20 17:10:23 +0100

    render ingredients passed from the builder component

--- src/Components/Burger/Burger.js
+++ src/Components/Burger/Burger.js
@@ -2,12 +2,23 @@ import React from "react";
 import classes from "./Burger.module.css";
 import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
 
-const burger = () => {
+const burger = props => {
+  let newIngredients = Object.keys(props.ingredients)
+    .map(igKey => {
+      return [...Array(props.ingredients[igKey])].map((_, index) => {
+        return <BurgerIngredient key={igKey + index} type={igKey} />;
+      });
+    })
+    .reduce((arr, el) => {
+      return arr.concat(el);
+    }, []);
+  if (newIngredients.length === 0) {
+    newIngredients = <p>Please start adding ingredients!</p>;
+  }
   return (
     <div className={classes.Burger}>
       <BurgerIngredient type="bread-top" />
-      <BurgerIngredient type="cheese" />
-      <BurgerIngredient type="meat" />
+      {newIngredients}
       <BurgerIngredient type="bread-bottom" />
     </div>
   );
