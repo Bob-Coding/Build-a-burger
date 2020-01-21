803f9b3342a543a01b9c280ea7d9030259d93bc2
Bob-Coding <bobgunsing@gmail.com>
2020-01-21 04:01:30 +0100

    fixed negative amount error  with removing ingredients and added rendering total price fixed to 2 decimals

--- src/Components/Burger/BuildControls/BuildControls.js
+++ src/Components/Burger/BuildControls/BuildControls.js
@@ -12,12 +12,16 @@ const controls = [
 const buildControls = props => {
   return (
     <div className={classes.BuildControls}>
+      <p>
+        TotalPrice: <strong>€{props.price.toFixed(2)}</strong>
+      </p>
       {controls.map(control => (
         <BuildControl
           key={control.label}
           label={control.label}
           added={() => props.ingredientAdded(control.type)}
           removed={() => props.ingredientRemoved(control.type)}
+          disabled={props.disabled[control.type]}
         />
       ))}
     </div>
