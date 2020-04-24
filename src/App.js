f40f86132b151b8a2f79b70cd2eebd5ec8eff335
Bob-Coding <bobgunsing@gmail.com>
2020-04-24 21:58:19 +0200

    added orders page, fixed navigationlinks

--- src/App.js
+++ src/App.js
@@ -2,17 +2,16 @@ import React from "react";
 import Layout from "./hoc/Layout/Layout";
 import BurgerBuilder from "./Containers/BurgerBuilder/BurgerBuilder";
 import Checkout from "./Containers/Checkout/Checkout";
+import Orders from "./Containers/Orders/Orders";
 import { Route, Switch } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <Layout>
-        {/*<Route path="/" exact component={BurgerBuilder} /> 
-        <Route path="/checkout" component={Checkout} />
-        The above method is also an option, but im using Switch and change the "/" route in order, so it wont be always rendered as a prefix standard*/}
         <Switch>
           <Route path="/checkout" component={Checkout} />
+          <Route path="/orders" component={Orders} />
           <Route path="/" exact component={BurgerBuilder} />
         </Switch>
       </Layout>
